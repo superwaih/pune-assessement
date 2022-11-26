f1410d8820a5f6d0caaa2f7b2ed9f824a0f82abf
shittu adewale <sheriffhardewale@gmail.com>
2022-11-26 14:53:25 +0100

    replaced dataset

--- pages/map/index.jsx
+++ pages/map/index.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import Map from "react-map-gl";
+import React, { useCallback, useEffect, useState } from "react";
+import Map, {Source, Layer} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 // Setting mapbox token
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoicnRkLXN1cGVybWFuIiwiYSI6ImNreTZ3bG03YjAzMTgyd2xqMWtrd2k2cm0ifQ.4VY5RaXBQD2En3LY1VlD4w";
 const index = () => {
-    console.log(Countries)
+
+  const[countriesData, setCountriesData] = useState([])
+    useEffect(() => {
+      fetch("https://raw.githubusercontent.com/superwaih/pune-assessement/main/data/countries.json")
+      .then(resp => resp.json())
+      .then(resp =>
+        setCountriesData(resp))
+    }, [])
+
+    const onHover = useCallback(event => {
+      const {
+        features,
+        point: {x, y}
+      } = event;
+      const hoveredFeature = features && features[0];
+  
+      // prettier-ignore
+      setHoverInfo(hoveredFeature && {feature: hoveredFeature, x, y});
+    }, []);
+
   return (
     <div>
      
@@ -30,10 +49,15 @@ const index = () => {
           latitude: 37.8,
           zoom: 14,
         }}
+        interactiveLayerIds={['countriesData']}
         mapboxAccessToken={MAPBOX_TOKEN}
         style={{ width: 1200, height: 1000 }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+      > 
+      <Source type="geojson" data={countriesData}>
+          <Layer />
+        </Source>
+      </Map>
     </div>
   );
 };
